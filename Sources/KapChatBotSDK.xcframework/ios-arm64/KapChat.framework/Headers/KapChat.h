d3a159c8b64e8a25ce2af0021de866f511448b2d
Lucas <lucas.charles@kapturecrm.com>
2024-10-29 12:37:46 +0530

    Initial Commit

---
+++ Sources/KapChatBotSDK.xcframework/ios-arm64/KapChat.framework/Headers/KapChat.h
@@ -0,0 +1,18 @@
+//
+//  KapChat.h
+//  KapChat
+//
+//  Created by Saksham Srivastava on 24/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KapChat.
+FOUNDATION_EXPORT double KapChatVersionNumber;
+
+//! Project version string for KapChat.
+FOUNDATION_EXPORT const unsigned char KapChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KapChat/PublicHeader.h>
+
+
